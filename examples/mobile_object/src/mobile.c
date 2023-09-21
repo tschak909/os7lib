798c257e4e227f3e20ad4f8a5f2386cf9f03b323
Thomas <thom.cherryhomes@gmail.com>
2023-09-21 10:32:35 -0500

    mobile object second attempt.

--- examples/mobile_object/src/mobile.c
+++ examples/mobile_object/src/mobile.c
@@ -9,7 +9,47 @@
 #include <arch/z80.h>
 #include <intrinsic.h>
 #include <os7.h> 
-#include "generators.h" // pac-man's data is here.
+#include "generators.h"
+
+/**
+ * @brief mode 1 color palette
+ * @verbose defined as static const so it stays in ROM, and is only referenced in this file.
+ */
+static const unsigned char colors[32]=
+  {
+    0xB4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0x64, // 5 for bonus
+    0x64,
+    0x64,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4,
+    0xF4
+  };
 
 /**
  * @brief The data structures for the timer table, to support one timer
@@ -17,108 +57,38 @@
 TimerTable tt[2];
 TimerData td[2];
 
-/**
- * @brief the size of writer queue is queue size / 3, as there are three bytes per entry
- */
-#define WRITER_QUEUE_SIZE 16
+unsigned char newgen[72];
+unsigned char queue[48];
+MOBStatus status;
+MOBOldScreen oldScreen;
 
-/**
- * @brief The queue needed to hold 16 deferred VDP write entries
- */
-unsigned char writer_queue[WRITER_QUEUE_SIZE*WRITER_QUEUE_ENTRY_SIZE];
+const MOBFrame frame = {0x00,0x01,0x02,0x03,0xB0};
+MOBGraphics(1) Graphics;
+const Graphics graphics = {MOBILE,4,newgen,pacman_right_horizontal,{frame}};
+const MOB mob = {graphics,status,oldScreen,0x10};
 
-/**
- * @brief PATTERN TABLE generators which will hold each frame, and color
- */
-const MOBFrame pacmanFrame0 = {0x00,0x01,0x02,0x03,0xB0};
-const MOBFrame pacmanFrame1 = {0x04,0x05,0x06,0x07,0xB0};
-const MOBFrame pacmanFrame2 = {0x08,0x09,0x0a,0x0b,0xB0};
-
-/**
- * @brief newGen is 9 generator spaces, each holding 8 bytes for temporary transformation storage.
- */
-unsigned char pacmanNewGen[NEWGEN_SIZE];
-
-/**
- * @brief Graphics object which points to bitmaps in ROM, and mappings to frames above
- */
-MOBGraphics(3) PacmanGraphics;
-
-const PacmanGraphics pacmanGraphics = {MOBILE, 3, pacmanNewGen, pacman_right_horizontal, {pacmanFrame0, pacmanFrame1, pacmanFrame2}};
-
-/**
- * @brief Old Screen object (backing store) for the PAC-MAN object
- */
-MOBOldScreen pacmanOldScreen;
-
-/**
- * @brief Pac-man's STATUS object which holds position, frame, etc.
- */
-MOBStatus pacmanStatus;
-
-/**
- * @brief PAC-Man's Top-level object
- */
-const MOB pacmanMOB = {pacmanGraphics,pacmanStatus,pacmanOldScreen,0x80};
-
-/**
- * @brief the routine to use during VDP interrupt, update writer and timer manager
- */
 static void vdp_nmi(void)
 {
-  M_PRESERVE_ALL; // preserve registers, so we don't crash
-  writer();       // fire off pending queue entries
-  time_mgr();     // decrement all timers
-  M_RESTORE_ALL;  // restore registers, so we don't crash
+  M_PRESERVE_ALL;
+  writer();
+  time_mgr();
+  M_RESTORE_ALL;
 }
 
-/**
- * @brief Initialize timer, writer, and VDP
- */
-void init(void)
-{
-  init_timer(&tt,&td);                          // Set up timer queue
-  init_writer(writer_queue,WRITER_QUEUE_SIZE);  // set up writer queue
-  add_raster_int(vdp_nmi);                      // attach vdp_nmi() to be called on every VDP interrupt.
-
-  mode_1();                                     // set up VDP mode 1, at this point, display is OFF (blank)
-  load_ascii();                                 // put ASCII table in PATTERN GENERATORS
-  fill_vram(MODE1_PATTERN_NAME_TABLE,768,' ');  // spaces in name table
-  fill_vram(MODE1_PATTERN_COLOR_TABLE,32,0xF0); // Fill color table with white foreground/black background
-  write_register(0x01,0xE0);                    // turn on display, mode 1, VDP interrupt.
-}
-
-/**
- * @brief Plot sign-on message to VDP using ASCII PATTERNs
- */
-void signon_msg(void)
-{
-  const char sign_on[32]="  MODE 1 MOBILE PATTERN TEST   ";
-
-  put_vram(PATTERN_NAME_TABLE,0x0000,sign_on,32);
-}
-
-/**
- * @brief Program starts here
- */
 void main(void)
 {
-  init();
-  signon_msg();
-  pacmanStatus.y=128;
-  activate(pacmanMOB,false);
-  
-  while(1)
-    {
-      SignalNum wait = request_signal(1,false);
-      pacmanStatus.x++;
-      pacmanStatus.frame++;
-      if (pacmanStatus.x>256)
-	pacmanStatus.x=0;
-      if (pacmanStatus.frame>2)
-	pacmanStatus.frame=0;
+  init_timer(&tt,&td);
+  init_writer(queue,16);
+  add_raster_int(vdp_nmi);
+  mode_1();
+  load_ascii();
+  game_opt();
+  put_vram(PATTERN_COLOR_TABLE,0,colors,sizeof(colors)); // Place the color palette.
+  write_register(0x07,0x04);                    // set border to dark blue.
+  blank(true);
 
-      put_obj(pacmanMOB,0);
-      while (!test_signal(wait));
-    }
+  activate(mob,false);
+  put_obj(mob,0);
+  
+  while(1);
 }
