7ed3139125a659f470185a970fd611f717dcd140
Thomas <thom.cherryhomes@gmail.com>
2023-09-27 12:45:27 -0500

    add scaffold for sound_demo

---
+++ examples/sound_demo/src/sound_demo.c
@@ -0,0 +1,16 @@
+/**
+ * @brief Complex Object demo (Mode 2)
+ * @author Thomas Cherryhomes
+ * @email thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3, see COPYING for details
+ */
+
+#include <os7.h>
+#include <interrupt.h>
+#include <arch/z80.h>
+#include <intrinsic.h>
+#include <stdbool.h>
+
+void main(void)
+{
+}
