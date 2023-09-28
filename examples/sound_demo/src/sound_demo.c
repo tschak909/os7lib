27fec02fb9b80a55724a77e6525e57293af9ae74
Thomas <thom.cherryhomes@gmail.com>
2023-09-27 21:09:26 -0500

    update sound demo

--- examples/sound_demo/src/sound_demo.c
+++ examples/sound_demo/src/sound_demo.c
@@ -10,7 +10,37 @@
 #include <arch/z80.h>
 #include <intrinsic.h>
 #include <stdbool.h>
+#include "sound_data_area.h"
+
+/**
+ * @brief The data structures for the timer table, to support one timer
+ */
+TimerTable tt[2];
+TimerData td[2];
+
+const char signon[32]="OS7 SOUND DEMONSTRATION";
+
+static void nmi()
+{
+  M_PRESERVE_ALL;
+  play_songs();
+  sound_man();
+  time_mgr();
+  VDP_STATUS_BYTE = read_register();
+  M_RESTORE_ALL;
+}
 
 void main(void)
 {
+  mode_1();
+  sound_init(1,&soundTable);
+  init_timer(&tt,&td);
+  add_raster_int(nmi);
+  load_ascii();
+  fill_vram(0x2000,32,0xF4);
+  write_register(0x07,0x04);
+  put_vram(PATTERN_NAME_TABLE,0x04,signon,sizeof(signon));
+  write_register(0x01,0xE0);
+  play_it(1);
+  while(1);
 }
